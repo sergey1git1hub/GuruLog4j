a25e88274cc2c43685123097a8bdb138b0a67cec
SChubuk <nameserhiy@gmail.com>
2017-10-24 18:07:16 +0300

    c1

---
+++ src/main/java/Log4J2XmlConfTest.java
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class Log4J2XmlConfTest {
+
+    @Test
+    public void testPerformSomeTask() throws Exception {
+        Log4J2XmlConf log4J2XmlConf = new Log4J2XmlConf();
+        log4J2XmlConf.performSomeTask();
+    }
+}
